8828310c58907ef88f0cb88afe02f362c0980f58
Al7Mino <al7.deniau@gmail.com>
2023-09-08 15:20:13 +0200

    feat: add custom format with test pokedex modified

---
+++ config/custom-formats.ts
@@ -0,0 +1,14 @@
+// Note: This is the list of formats
+// The rules that formats use are stored in data/rulesets.ts
+
+export const Formats: FormatList = [
+	{
+		section: "Custom Format",
+	},
+	{
+		name: "[Gen ?] Special format",
+		mod: 'custom',
+		ruleset: ['Standard'],
+		banlist: ['Uber', 'AG', 'Arena Trap', 'Moody', 'Sand Veil', 'Shadow Tag', 'Snow Cloak', 'King\'s Rock', 'Baton Pass', 'Last Respects', 'Shed Tail'],
+	},
+];
--- config/formats.ts
+++ config/formats.ts
@@ -17,7 +17,10 @@ New sections will be added to the bottom of the specified column.
 The column value will be ignored for repeat sections.
 */
 
-export const Formats: FormatList = [
+// Use only custom format
+export const Formats: FormatList = []
+
+/* export const Formats: FormatList = [
 
 	// S/V Singles
 	///////////////////////////////////////////////////////////////////
@@ -4433,4 +4436,4 @@ export const Formats: FormatList = [
 		battle: {trunc: Math.trunc},
 		ruleset: ['HP Percentage Mod', 'Cancel Mod', 'Desync Clause Mod', 'Max Team Size = 24', 'Max Move Count = 24', 'Max Level = 9999', 'Default Level = 100'],
 	},
-];
+]; */
